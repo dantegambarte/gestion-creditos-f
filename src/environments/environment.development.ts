5611329c33b19014c0d93118f6d797d7b3eeb24f
dantegambarte <da.nt3@hotmail.com>
2026-07-03 00:39:54 -0300

    chore: point dev environment to devtunnel API

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: false,
   useMocks: false,
-  apiBaseUrl: 'http://localhost:3000/api',
+  // apiBaseUrl: 'http://localhost:3000/api',
+  apiBaseUrl: 'https://cxsfb55j-3000.brs.devtunnels.ms/api',
   tokenKey: 'sgcf_token',
   portalTokenKey: 'sgcf_portal_token',
 };
